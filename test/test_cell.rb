cd68308f5f7116a4cb2c14f2a6404b62b8674f19
David Vargas <dvarrui@protonmail.com>
2025-10-13 22:39:58 +0100

    N cell test

---
+++ test/test_cell.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class CellTest < Test::Unit::TestCase
+  test "New cell" do
+    cell = Cell.new
+    assert_equal(true, cell.empty?)
+
+    #assert do
+    #  ::Word::Search::Puzzle.const_defined?(:VERSION)
+    #end
+  end
+
+  test "Push and pull" do
+    cell = Cell.new
+    cell.push('A')
+    assert_equal("A", cell.data)
+    assert_equal(false, cell.empy?)
+    data = cell.push
+    assert_equal(true, cell.empy?)
+    assert_equal("A", data)
+  end
+end
