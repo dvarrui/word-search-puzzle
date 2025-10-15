7708fb893b0b9e01c6e87108516961c22c577dc1
David Vargas <dvarrui@protonmail.com>
2025-10-15 10:29:10 +0100

    [U]

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -3,24 +3,36 @@
 require "test_helper"
 
 class CellTest < Test::Unit::TestCase
-  test "New cell" do
+  test "New" do
     cell = Cell.new
     assert_equal(true, cell.empty?)
+    assert_equal(0, cell.count)
 
     # assert do
     #  ::Word::Search::Puzzle.const_defined?(:VERSION)
     # end
   end
 
+  test "Push" do
+    cell = Cell.new
+    cell.push("A")
+
+    assert_equal("A", cell.data)
+    assert_equal(1, cell.count)
+    assert_equal(false, cell.empty?)
+  end
+
   test "Push and pull" do
     cell = Cell.new
-
     cell.push("A")
+
     assert_equal("A", cell.data)
+    assert_equal(1, cell.count)
     assert_equal(false, cell.empty?)
 
     data = cell.pull
-    assert_equal(true, cell.empty?)
     assert_equal("A", data)
+    assert_equal(0, cell.count)
+    assert_equal(true, cell.empty?)
   end
 end
