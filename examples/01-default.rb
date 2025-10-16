34789bd32e04148a57658b15dfd02225e685406a
David Vargas <dvarrui@protonmail.com>
2025-10-16 21:57:21 +0100

    N create examples

--- examples/01-default.rb
+++ examples/01-default.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
-require "debug"
-require_relative "lib/word-search-puzzle"
+require_relative "../lib/word-search-puzzle"
 
-# words = %w[RED BLUE YELLOW GREEN BLACK WHITE PINK PURPLE ORANGE BROWN GREY]
 words = %w[RED BLUE YELLOW GREEN BLACK WHITE PINK PURPLE ORANGE BROWN GREY]
 
 puzzle = WordSearchPuzzle.create(
   words: words, 
   size: 10)
+
 puts puzzle.render
---
+++ examples/02-solution.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+require_relative "../lib/word-search-puzzle"
+
+words = %w[RED BLUE YELLOW GREEN BLACK WHITE PINK PURPLE ORANGE BROWN GREY]
+
+puzzle = WordSearchPuzzle.create(
+  words: words, 
+  size: 10)
+
+puts puzzle.render(color: true, padding: ['.'])
