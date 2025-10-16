336548fb419afb95a57e560fa95f171484a1d28e
David Vargas <dvarrui@protonmail.com>
2025-10-16 11:35:15 +0100

    [U]

--- main.rb
+++ main.rb
@@ -8,6 +8,6 @@ words = %w[RED BLUE YELLOW GREEN BLACK WHITE PINK PURPLE ORANGE BROWN GREY]
 puzzle = WordSearchPuzzle.create(
   words: words, 
   size: 10, 
-  locks: [[1,1], [1,2], [2,1], [2,2]]
+  locks: [[0,0], [0,1], [0,2], [1,0], [2,0], [1,1]]
   )
 puts puzzle.render
