56e72eeaabe94c7402d80fac5a1967be2da1be62
David Vargas <dvarrui@protonmail.com>
2025-10-19 16:56:08 +0100

    Fix

--- examples/02-read_words_file.rb
+++ examples/02-read_words_file.rb
@@ -3,7 +3,7 @@
 require_relative "../lib/word-search-puzzle"
 
 # Create puzzle (size 10x10) reading words list from text file
-filename = File.join(__dir___, "starwars.txt")
+filename = File.join(File.dirname(__FILE__), "starwars.txt")
 puzzle = WordSearchPuzzle.create(words: filename)
 
 # Check if exits solution
