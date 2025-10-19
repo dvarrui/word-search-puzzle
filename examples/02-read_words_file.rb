6277d8aff8d4fc0f97781037a176515bc5862a7d
David Vargas <dvarrui@protonmail.com>
2025-10-19 16:55:23 +0100

    Fix paht to shape file

--- examples/02-read_words_file.rb
+++ examples/02-read_words_file.rb
@@ -3,7 +3,7 @@
 require_relative "../lib/word-search-puzzle"
 
 # Create puzzle (size 10x10) reading words list from text file
-filename = "starwars.txt"
+filename = File.join(__dir___, "starwars.txt")
 puzzle = WordSearchPuzzle.create(words: filename)
 
 # Check if exits solution
