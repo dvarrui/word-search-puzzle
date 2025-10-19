8556a2c0509ad2cd3fc654935f486531ce5b8022
David Vargas <dvarrui@protonmail.com>
2025-10-19 12:28:35 +0100

    [U]

--- test/input_test.rb
+++ test/input_test.rb
@@ -6,17 +6,25 @@ class InputTest < Test::Unit::TestCase
   test "Read words: string with words list" do
     data = "JEDI,SITH,STARWARS"
     words = WordSearchPuzzle::Input.read_words(data)
-
     assert_equal(data.split(","), words)
   end
 
   test "Read words: string with spaces between words list" do
     data = "JEDI, SITH , STARWARS"
     words = WordSearchPuzzle::Input.read_words(data)
-
     assert_equal(["JEDI", "SITH", "STARWARS"], words)
-
-    # size = WordSearchPuzzle::Input.read_size(size)
-    # gaps = WordSearchPuzzle::Input.read_gaps(gaps)
   end
+
+  test "Read words: empty list" do
+    words = WordSearchPuzzle::Input.read_words("")
+    assert_equal([], words)
+  end
+
+  test "Read words: nil list" do
+    words = WordSearchPuzzle::Input.read_words(nil)
+    assert_equal([], words)
+  end
+
+  # size = WordSearchPuzzle::Input.read_size(size)
+  # gaps = WordSearchPuzzle::Input.read_gaps(gaps)
 end
