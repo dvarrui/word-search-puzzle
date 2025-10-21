1c907bef40e0e4cab8f1d10e3e98e9db2eef5ef0
David Vargas <dvarrui@protonmail.com>
2025-10-21 18:29:16 +0100

    [U]

--- examples/03-render_options.rb
+++ examples/03-render_options.rb
@@ -4,10 +4,10 @@ require_relative "../lib/word-search-puzzle"
 # 1. Define your word list
 words = %w[STARWARS OBIWAN LUKE VADER R2D2 C3PO LEIA YODA EMPERADOR SITH]
 
-# 2. Create your puzzle
+# 2. Create your puzzle (10x20 size)
 puzzle = WordSearchPuzzle.create(
   words: words,
-  size: 10
+  size: "10x20"
 )
 
 # 3. Show puzzle on screen:
